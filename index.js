3a65d52e730ebd37b6cca90ea14eb00fb8a21da4
kakukaku-15 <s18t304@stu.kagawa-u.ac.jp>
2019-05-31 21:18:24 +0900

    message

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+
 var express = require("express");
 var app = express();
+var http = require("http").Server(app);
+const io = require("socket.io")(http);
+const PORT = process.env.PORT || 3000;
 
-var server = app.listen(3000, function(){
-    console.log("Node.js is listening to PORT:" + server.address().port);
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/", function(req, res){
-    console.log('HELLOOOOO');
-    res.sendFile(__dirname + '/views/index.html');
+io.on("connection", function(socket) {
+    socket.on("message", function(msg) {
+        console.log("message: " + msg);
+        io.emit("message", msg);
+    });
+});
+
+http.listen(PORT, function() {
+    console.log("server listening. Port:" + PORT);
 });
