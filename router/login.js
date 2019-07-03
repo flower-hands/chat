9687083972543fe48231fc20e47e652249db4e00
ikabust <ikabust.baki1011@gmail.com>
2019-07-03 21:12:50 +0900

    ログイン画面からチャット画面への遷移をした。

--- router/login.js
+++ router/login.js
@@ -1,19 +1,19 @@
 const express = require("express");
-const router = express.Router();
+const router  = express.Router();
 
 //router.use(express.static("./../../views"));
 //router.use(express.static("./test.html"));
-
 
 router.get("/", (req, res) => {
     console.log(__dirname);
     dir = __dirname
     dir = dir.slice(0, dir.length - 6)
     //res.sendFile("C:/Users/18t304/Desktop/chat/views/index.html");
-    res.sendFile(dir + "views/index.html");
+    res.sendFile(dir + "views/login.html");
     console.log(dir);
     
     //res.send("aaa");
 });
+
 
 module.exports = router;
