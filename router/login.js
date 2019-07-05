b4619fb103f999ef8a9870246ab68957b0eeaa5c
kakukaku-15 <s18t304@stu.kagawa-u.ac.jp>
2019-07-05 21:09:52 +0900

    チャット画面からログイン画面への画面遷移をした。

--- router/login.js
+++ router/login.js
@@ -5,15 +5,20 @@ const router  = express.Router();
 //router.use(express.static("./test.html"));
 
 router.get("/", (req, res) => {
-    console.log(__dirname);
+    //console.log(__dirname);
     dir = __dirname
     dir = dir.slice(0, dir.length - 6)
     //res.sendFile("C:/Users/18t304/Desktop/chat/views/index.html");
     res.sendFile(dir + "views/login.html");
-    console.log(dir);
+    //console.log(dir);
     
     //res.send("aaa");
 });
 
+router.post("/", (req, res) => {
+    dir = __dirname
+    dir = dir.slice(0, dir.length - 6)
+    res.sendFile(dir + "views/login.html");
+});
 
 module.exports = router;
